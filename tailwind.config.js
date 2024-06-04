4aff4c8883b3e70716802e07b982423fb41c4a10
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-04 23:13:11 +0600

    fonts are added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      fontFamily: {
+        chillax: ["Chillax", "sans-serif"],
+        gambetta: ["Gambetta", "sans-serif"],
+        satoshi: ["Satoshi", "sans-serif"],
+      },
     },
   },
   plugins: [],
