37939747af1c7c5f3ae9e925db468349e14fcad7
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-13 20:34:54 +0600

    button hover text color fixed

--- src/app/components/Button/Button.jsx
+++ src/app/components/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   const { children, className, ...rest } = props;
   return (
     <button
-      className={`rounded-sm border-2 border-bg-primary bg-bg-primary px-4 py-[0.625rem] text-meta uppercase text-text-inverse transition duration-500 hover:bg-transparent hover:text-white ${className}`}
+      className={`rounded-sm border-2 border-bg-primary bg-bg-primary px-4 py-[0.625rem] text-meta uppercase text-text-inverse transition duration-500 hover:bg-transparent hover:text-text ${className}`}
       {...rest}
     >
       {children}
