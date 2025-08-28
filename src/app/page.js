c01177c96cd3b331fc4456242e42898db6da29ee
shakib-mia <smdshakibmia2001@gmail.com>
2025-08-28 13:14:09 +0600

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
-// "use client";
-import Layout from "./components/Layout/Layout";
 import Header from "./components/Header/Header";
-// import useLenis from "./hooks/useLenis";
-// import useAOS from "./hooks/useAos";
 import Clients from "./components/Clients/Clients";
 import Services from "./components/Services/Services";
 import SelectedWorks from "./components/SelectedWorks/SelectedWorks";
@@ -12,9 +8,6 @@ import About from "./components/About/About";
 import Head from "./head";
 
 export default function Home() {
-  // useLenis();
-  // useAOS();
-
   return (
     <>
       <Head
